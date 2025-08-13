45bc28215b0dba8afb53a50e2e8ac937d4884247
Shinya ( Shin , Shaun, Shun) <47809066+saitoshi@users.noreply.github.com>
2025-08-14 08:56:29 +0900

    added jsonToCSV Function

--- clientCompanyManagement/jsonToCSV.js
+++ clientCompanyManagement/jsonToCSV.js
@@ -4,11 +4,27 @@
  * @param jsonObj - The overall json object 
  * @return success or fail 
  */
-const jsonToCSV = async () => {
+const jsonToCSV = async (jsonArray, fileName) => {
     try {
-
+        const fs = require("fs");
+        const path = require("path");
+        let filePath = `./csvFiles/${fileName}.csv`
+        const dir = path.dirname(filePath);
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true });
+        }
+        if (!Array.isArray(jsonArray) || jsonArray.length === 0) {
+            return "Not an array or array is empty"
+        }
+        const headers = Object.keys(jsonArray[0]);
+        const csvRows = [headers.join(","), ...jsonArray.map((row) => headers.map((field) => JSON.stringify(row[field] ?? "")).join(",")),];
+        let csvContent = csvRows.join("\n");
+        await fs.writeFileSync(filePath, csvContent, { encoding: "utf-8" });
+        return filePath;
     } catch (error) {
-        console.error(error);
+        console.error("Error Converting JSON to CSV: " + error.message);
         return error;
     }
 }
+
+module.exports = jsonToCSV;
